d19e75ab15cdfb7e1d02c38ac098b7019bfa0243
ProsperousJoy <rizkysaputraamr006@gmail.com>
2024-09-25 10:41:27 +0700

    Update endpoint.py

--- Model/endpoint.py
+++ Model/endpoint.py
@@ -1,8 +1,15 @@
 import pickle
 import numpy as np
+from flask import Flask, request
 
 filename = "Model/GradesRF.sav"
 model = pickle.load(open(filename, "rb"))
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print("adhuadha")
 
 input_data = np.array([[5, 0, 3, 1, 1, 1, 90, 65, np.nan]])
 
