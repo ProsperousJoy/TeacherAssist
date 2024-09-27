f59457be7ff5035d3b8d13e5eca4c421cf0d958b
ProsperousJoy <rizkysaputraamr006@gmail.com>
2024-09-28 02:51:09 +0700

    restructured and added comments

--- Model/Gemini/sendPrompt.py
+++ Model/Gemini/sendPrompt.py
@@ -1,9 +1,11 @@
 from GeminiAIChat.core import API
-import os
 from dotenv import load_dotenv
+import os
 
+# Create your .env file in root folder and it will be loaded here
 load_dotenv()
 
+# Prompts
 res = API(os.environ['GEMINI_API_KEY'])
 res.prompt("define what is python")
 print(res.response())
